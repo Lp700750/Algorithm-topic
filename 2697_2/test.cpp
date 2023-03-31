effdd1e8de4fce58194d9db40e40b674b8033e33
Lp700750 <2357543706@qq.com>
2023-03-31 19:10:58 +0800

    LeetCode测试

---
+++ 2697_2/test.cpp
@@ -0,0 +1,22 @@
+class Solution 
+{
+public:
+    vector<int> exchange(vector<int>& nums) 
+    {
+        int i=0;
+        int j=nums.size()-1;
+        while(i<j)
+        {
+            while(i<j&&nums[i]%2==1)
+            {
+                i++;
+            }
+            while(i<j&&nums[j]%2==0)
+            {
+                j--;
+            }
+            std::swap(nums[i],nums[j]);
+        }
+        return nums;
+    }
+};
